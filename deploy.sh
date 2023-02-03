f5f417fa5304c150a3cf3a08d655d343aa7d49e6
ilyas <elaissiilyas@gmail.com>
2023-02-03 21:29:12 +0100

    ...

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# if you are deploying to https://IlyasDevSample.github.io/IlyasDevSample
 # git push -f git@github.com:IlyasDevSample/IlyasDevSample.git main:gh-pages
 
 cd -
